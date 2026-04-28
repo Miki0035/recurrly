9ad0f705b8f33f5221b035cadfe5253c4c2eef73
Miki0035 <mknewkebede@gmail.com>
2026-04-28 19:11:00 +0300

    feat: add working bottom nav

--- lib/features/bottom_nav_bar.dart
+++ lib/features/bottom_nav_bar.dart
@@ -1,22 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:recurrly/core/constants/colors.dart';
+import 'package:recurrly/features/home/presentation/screens/home_screen.dart';
+import 'package:recurrly/features/insight/presentation/screens/insight_screen.dart';
+import 'package:recurrly/features/setting/presentation/screens/setting_screen.dart';
+import 'package:recurrly/features/subscription/presentation/screens/subscription_screen.dart';
 
-class RBottomNavBar extends StatelessWidget {
+class RBottomNavBar extends StatefulWidget {
   const RBottomNavBar({super.key});
+
+  @override
+  State<RBottomNavBar> createState() => _RBottomNavBarState();
+}
+
+class _RBottomNavBarState extends State<RBottomNavBar> {
+  final List<Widget> _pages = [
+    HomeScreen(),
+    SubscriptionScreen(),
+    InsightScreen(),
+    SettingScreen(),
+  ];
+
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: RColor.lightBeige,
+      body: _pages[_selectedIndex],
       bottomNavigationBar: SafeArea(
-        child: GNav(
-          tabs: [
-            GButton(icon: Icons.home),
-            GButton(icon: Icons.wallet),
-            GButton(icon: Icons.show_chart),
-            GButton(icon: Icons.settings),
-          ],
+        child: Container(
+          height: 90,
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(50),
+            child: GNav(
+              color: Colors.white,
+              backgroundColor: RColor.darkBlack,
+              tabBackgroundColor: RColor.orange,
+              activeColor: Colors.white,
+              tabBorderRadius: 50,
+              selectedIndex: _selectedIndex,
+              gap: 5,
+              onTabChange: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              tabs: [
+                GButton(
+                  icon: Icons.home,
+                  padding: .all(12),
+                  margin: .only(left: 12),
+                ),
+                GButton(icon: Icons.wallet, padding: .all(12)),
+                GButton(icon: Icons.show_chart, padding: .all(12)),
+                GButton(
+                  icon: Icons.settings,
+                  padding: .all(12),
+                  margin: .only(right: 12),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
